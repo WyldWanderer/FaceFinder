0ac1023b061812b200761644a1b4e81ff4967743
Tyler <living2snowboard@gmail.com>
2020-05-28 17:25:22 -0600

    FaceRecognition.css added, functionality to create boundary box added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import Navigation from './components/Navigation/Navigation.js';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition.js';
-import Logo from './components/Logo/Logo.js'
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js'
-import Rank from './components/Rank/Rank.js'
+import Logo from './components/Logo/Logo.js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './components/Rank/Rank.js';
 import './App.css';
-import 'tachyons'
+import 'tachyons';
 import Particles from 'react-particles-js';
-import Clarifai from "clarifai"
+import Clarifai from "clarifai";
 
 const app = new Clarifai.App({
   apiKey: "b3b9e57a6b734f5897847c4888965ff1"
@@ -41,8 +41,28 @@ class App extends Component {
     super();
     this.state = {
       input: "",
-      imageUrl: ""
+      imageUrl: "", 
+      box: {},
     }
+  }
+
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height),
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box)
+    this.setState({box: box})
   }
 
   onSubmit = () => {
@@ -51,35 +71,31 @@ class App extends Component {
       .predict(
         Clarifai.FACE_DETECT_MODEL, 
         this.state.input)
-        .then(
-        function(response) {
-          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-        },
-        function(err) {
-          // there was an error
-      }
-    );
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
   }
 
   onInputChange = (event) => {
     this.setState({input: event.target.value});
   }
-  render(){
+
+  render() { 
     return (
       <div className="App">
         <Particles className='particles'
               params={particlesOptions}
-            />
+        />
         <Navigation />
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} 
         onSubmit={this.onSubmit}/>
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
-
   }
-} 
+}
+
+ 
 
 export default App;
