7ba7f66918b32d03d1a772c16bdea02636feef14
Tyler <living2snowboard@gmail.com>
2020-07-21 22:14:16 -0600

    Code review changes to clean up App file

--- src/App.js
+++ src/App.js
@@ -38,23 +38,26 @@ const particlesOptions = {
   }
 }
 
+const initialState = {
+    input: "",
+    imageUrl: "", 
+    box: {},
+    route: 'signin',
+    isSignedIn: false,
+    user : {
+      id: '',
+      name: '',
+      email: '',
+      entries: 0,
+      joined: ''
+    }
+  }
+
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: "",
-      imageUrl: "", 
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user : {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = initialState;
+    
   }
 
   loadUser = (data) => {
@@ -103,6 +106,7 @@ class App extends Component {
             .then(count => {
               this.setState(Object.assign(this.state.user, { entries : count}))
             })
+            .catch(console.log)
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
@@ -115,7 +119,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
