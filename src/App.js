b5dd06e65586b4f817fcf31b1c1373be91bc4a46
Tyler <living2snowboard@gmail.com>
2020-07-22 21:57:16 -0600

    Changes made as part of security audit

--- src/App.js
+++ src/App.js
@@ -89,11 +89,15 @@ class App extends Component {
 
   onSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL, 
-        this.state.input)
-      .then(response => {
+      fetch('http://localhost:3000/imageurl', { 
+              method: 'post',
+              headers: {'Content-Type': 'application/json'},
+              body: JSON.stringify({
+                  input: this.state.input
+                })
+            })
+      .then(response => response.json())
+      .then ( response => {
         if (response) {
           fetch('http://localhost:3000/image', { 
             method: 'put',
